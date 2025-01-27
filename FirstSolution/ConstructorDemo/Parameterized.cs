3a65ae31df694eb77fcf57b7897199f558be8ffa
manudp9015 <manudp9015@gmail.com>
2025-01-27 18:04:42 +0530

    Add .gitignore to ignore .vs directory

---
+++ FirstSolution/ConstructorDemo/Parameterized.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo
+{
+    internal class Parameterized
+    {
+        int x;
+        Parameterized(int i)
+        {
+            x = i;
+        }
+        void display()
+        {
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            Parameterized p = new Parameterized(10);
+            p.display();
+
+
+        }
+    }
+}
---
+++ FirstSolution/FirstProject/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i am class 2");
+
+        }
+    }
+}
