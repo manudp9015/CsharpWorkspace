da2cd0d213897167f1e5bda98bf362a6bf9a362f
manudp9015 <manudp9015@gmail.com>
2025-01-30 18:23:55 +0530

    Abstract, Interface, Enum, Method Hidding , Multilevel Inheritance

--- Calculatorsolution/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculatorsolution/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04be45142d9f705bc02fe6d2465067056cb6fdc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+424b5230b3cc92c13abeb139a538e6d6ffe96437")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EnumerationSolution/Enumerationdemo1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+
+namespace Enumerationdemo1
+{
+
+
+    class Program
+    {
+        public enum  Days
+        {
+            Monday,Tuesday,Wednesday,Thursday,Friday
+        }
+
+        Days meetingDate { get; set; } = (Days)4;
+
+        public void Dispaly()
+        {
+            Console.WriteLine("meetingday is: " + meetingDate);
+        }
+        public static void Main()
+        {
+            Program program = new Program();
+            program.Dispaly();
+        }
+    }
+}
