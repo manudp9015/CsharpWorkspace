7e5874d23817bc636adb1cc59cccedfac70e52f7
manudp9015 <manudp9015@gmail.com>
2025-01-31 17:28:34 +0530

    method override using extension

---
+++ ExtensionSolutionm/Extensiondemo2MethodOverride/Static Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensiondemo
+{
+    internal static class StaticClass
+    {
+        public static void Test2(this Program p)
+        {
+            Console.WriteLine("method 3");
+        }
+        public static void Main()
+        {
+            Program p = new Program();
+            p.Test1();
+            p.Test2();
+           
+        }
+
+    }
+}
+
