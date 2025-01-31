f14a1da6719adb5c9064d0879a8e8986d4c83b8c
manudp9015 <manudp9015@gmail.com>
2025-01-31 17:48:09 +0530

    structure in charp demo examples

---
+++ StructureSolution/Structuredemo/CodeFile1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Structuredemo
+{
+    class CodeFile1
+    {
+
+        int number;
+
+        public CodeFile1(int number)
+        {
+            this.number = number;
+            Console.WriteLine("value is "+ number);
+        }
+        public static void Main()
+        {
+            CodeFile1 codefile = new CodeFile1(5);
+
+
+            Console.WriteLine();//parameter less constructor gav e by program implicitly
+            codefile.number = 6;
+            Console.WriteLine(codefile.number);
+
+
+            // struct cannot support inheritance
+            //struct only used for simple program
+            //we cannot ctreate parametrless constructor explicitly
+            //new keyword is optional
+
+        }
+    }
+}
