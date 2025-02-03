c955cc9c9237c73afd2b24898b3ba4c1c0d637b0
manudp9015 <manudp9015@gmail.com>
2025-02-03 15:35:57 +0530

    HashTable example demo

---
+++ CollectionSolution/HashTabledemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTabledemo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("name", "manu");
+            ht.Add("age", 21);
+            ht.Add("job", "developer");
+            ht.Add("company", "datalyzer");
+
+            foreach(object  i in ht.Keys)
+            {
+                Console.WriteLine(i+" "+ ht[i]);
+            }
+            Console.WriteLine();
+
+
+            ht.Remove("company");
+            foreach (object i in ht.Keys)
+            {
+                Console.WriteLine(i + " " + ht[i]);
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
