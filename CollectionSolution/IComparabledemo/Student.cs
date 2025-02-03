1426cd608efa9eb40679ce674c9847be4ea7eede
manudp9015 <manudp9015@gmail.com>
2025-02-03 17:24:55 +0530

    sort the collection for user defined type using IComaparable

---
+++ CollectionSolution/IComparabledemo/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Collections;
+
+namespace IComparabledemo
+{
+    class Student:IComparable<Student>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int clas { get; set; }
+        public int marks { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            if(this.Id < other.Id) return -1;
+            else if(this.Id > other.Id) return 1;
+            else return 0;
+        }
+    }
+    class StudentTest
+    {
+        public static void Main()
+        {
+            Student s1 = new Student{Id = 1, Name = "manu",clas=10,marks=544};
+            Student s2 = new Student { Id = 2, Name = "rohith", clas = 10, marks = 555 };
+            Student s3 = new Student { Id = 3, Name = "bharath", clas = 10, marks = 444 };
+            Student s4 = new Student { Id = 4, Name = "sagar", clas = 10, marks = 500 };
+
+            List<Student> list = new List<Student>(){ s1, s3, s2, s4 };
+
+            list.Sort();
+
+            foreach(Student student in list)
+            {
+                Console.WriteLine(+student.Id + " "+student.Name+" "+student.clas+" "+student.marks);
+            }
+
+
+
+
+
+        }
+    }
+}
+
+
