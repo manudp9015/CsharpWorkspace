5b4314fa42b6b012199cbcbad72f9ae536023adc
manudp9015 <manudp9015@gmail.com>
2025-02-03 10:57:50 +0530

    Exception Handling Example

---
+++ ExceptionSolution/ExceptionHandlingdemo1/ExceptionHandling1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Exceptiondemo1
+
+{
+    class Exception1
+    {
+
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("enter first number: ");
+                int first = int.Parse(Console.ReadLine());
+
+                Console.Write("enter second number: ");
+                int second = int.Parse(Console.ReadLine());
+
+                int z = first / second;
+                Console.WriteLine("The Value Of z is: " + z);
+            }
+            catch(DivideByZeroException e1) 
+            {
+                Console.WriteLine(e1.Message);
+            }
+            catch (FormatException e2)
+            {
+                Console.WriteLine(e2.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("The End of the Program.");
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
