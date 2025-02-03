40805f1ea074e491126f3aeade68b4d1276a1300
manudp9015 <manudp9015@gmail.com>
2025-02-03 15:34:27 +0530

    Array List example

---
+++ CollectionSolution/Collectiondemo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using  System.Collections;
+
+namespace Collectiondemo1
+{
+    class Program
+    {
+        
+        public static void Main ()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            al.Add(5);
+
+            foreach (int i in al) 
+                Console.Write(i+" ");
+            Console.WriteLine();
+
+            Console.WriteLine(al.Capacity);
+
+            al.Remove(4);
+            foreach (int i in al)
+                Console.Write(i+" ");
+
+            Console.WriteLine();
+
+
+                al.Insert(3,4);
+            foreach (int i in al)
+                Console.Write(i+" ");  
+        }
+    }
+}
