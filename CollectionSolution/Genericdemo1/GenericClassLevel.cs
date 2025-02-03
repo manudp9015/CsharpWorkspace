602cce9ae0bbb5a7494858b8f87c93b948be78eb
manudp9015 <manudp9015@gmail.com>
2025-02-03 15:36:59 +0530

    Generic object example demo

---
+++ CollectionSolution/Genericdemo1/GenericClassLevel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericdemo1
+{
+    public class GenericClassLevel <T>
+    {
+        public void add(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine("addition value is: " + (d1 + d2));
+        }
+        public void sub(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine("subtraction value is: " + (d1 - d2));
+        }
+        public void mul(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine("multiplication value is: " + (d1 * d2));
+        }
+        public void div(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine("division value is: " + (d1 / d2));
+        }
+
+        public static void Main()
+        {
+            GenericClassLevel<int> lt = new GenericClassLevel<int>();
+            lt.add(10, 20);
+            lt.sub(20, 30);
+            lt.mul(20, 30);
+            lt.div(30, 30);
+        }
+    }
+}
