3fbc667e0fbd628858e5a393486848d51cce4c79
manudp9015 <manudp9015@gmail.com>
2025-02-03 10:57:02 +0530

    Exceptiondemo example

---
+++ ExceptionSolution/Exceptiondemo/Exception1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Exceptiondemo1
+
+{
+    class Exception1
+    {
+
+        public static void Main()
+        {
+            Console.Write("enter first number: ");
+            int first=int.Parse(Console.ReadLine());
+
+            Console.Write("enter second number: ");
+            int second = int.Parse(Console.ReadLine());
+
+            int z = first / second;
+            Console.WriteLine("The Value Of z is: " + z);
+            Console.WriteLine("The End of the Program.");
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
