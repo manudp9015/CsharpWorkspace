058599ac90e5557e4993171207838c23d5209836
manudp9015 <manudp9015@gmail.com>
2025-02-04 12:02:25 +0530

    simple linq example demo

---
+++ LinqSolution/Linqdemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Linq;
+
+
+namespace Linqdemo
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int [] arr = { 10, 67, 12, 56, 93, 47, 27, 48, 95, 8, 3, 38 };
+            var brr=from i in arr where i > 40 select i;
+
+            foreach(var i in brr)
+            {
+                Console.Write(i+" ");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
