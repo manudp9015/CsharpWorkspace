7429d1f8a0d0fbcd025ee1f590134a25d38660ad
manudp9015 <manudp9015@gmail.com>
2025-02-11 14:05:21 +0530

    Time function Example demo

---
+++ TimerFunctionsSolution/TimerFunctionsDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Timers;
+
+class Program
+{
+    static void Main()
+    {
+        System.Timers.Timer timer = new System.Timers.Timer(1000);  
+        timer.Elapsed += OnTimerElapsed;
+        timer.Start();
+
+        Console.ReadLine();
+    }
+
+    static void OnTimerElapsed(object s, ElapsedEventArgs e)
+    {
+        Console.WriteLine("System.Timers.Timer event: " + DateTime.Now);
+    }
+}
