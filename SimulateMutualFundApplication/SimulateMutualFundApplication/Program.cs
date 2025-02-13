a7df99cdf39d4f888af6f76a6dc38bc8b9d901cb
manudp9015 <manudp9015@gmail.com>
2025-02-13 18:26:09 +0530

    Mutual Fund Simulator Project

---
+++ SimulateMutualFundApplication/SimulateMutualFundApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace MutualFundSimulatorApplication
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            MutualFundSimulatorUtility mutualFundSimulatorUtility = new MutualFundSimulatorUtility();
+            mutualFundSimulatorUtility.MainMenu();
+        }
+    }
+}
+
