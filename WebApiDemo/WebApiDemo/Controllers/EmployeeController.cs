9bdd90dec4c09866343ad14fc7f5024634685e3d
manudp9015 <manudp9015@gmail.com>
2025-02-26 18:18:04 +0530

    example api

---
+++ WebApiDemo/WebApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Data;
+using WebApiDemo.Models.Entity;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class EmployeeController:ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+           // var allEmployees = dbContext.Emplyees.ToList();
+
+            //return Ok(allEmployees);
+            return Ok(dbContext.Emplyees.ToList());
+        }
+        [HttpPut]
+        public IActionResult AddEmployee(EmployeeDto employeeDto)
+        {
+            var addEmployeeEntity = new Employee() { 
+                Phone = employeeDto.Phone,
+                Name = employeeDto.Name,
+                Salary = employeeDto.Salary,
+                Email = employeeDto.Email
+                };
+            dbContext.Emplyees.Add(addEmployeeEntity);
+            dbContext.SaveChanges();
+            return Ok(addEmployeeEntity);
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Models/Entity/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiDemo.Models.Entity
+{
+    public class Employee
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+
+
+    }
+}
